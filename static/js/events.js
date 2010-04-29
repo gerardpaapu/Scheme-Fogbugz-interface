1b4560da3e3dda92f0c32110dab8441c10dcbdd3
Gerard <gerard@pe-staff-09.(none)>
2010-04-30 11:39:58 +1200

    added a few more instant effects in js

--- static/js/events.js
+++ static/js/events.js
@@ -5,9 +5,28 @@ window.addEvent('domready', function (){
             if (!this.hasClass('disabled')) {
                 this.addClass('disabled');
 
-                var url = new URI(this.get('href')).setData({'format': "bare-html"});
-                $('MainContainer').load(url.toString());
+                updateView(this.get('href'));
+                if (this.hasClass('startWork')) setCurrent(this); 
+                if (this.get('id') == 'StopWork') stopWork(); 
             }
         }
     });
+
+    function updateView(url){
+        var url = new URI(url).setData({'format': "bare-html"});
+        $('MainContainer').load(url.toString());
+    }
+
+    function setCurrent(link){
+        $$('.case.current').removeClass('current');
+        link.getParent('li').addClass('current');
+    }
+
+    function stopWork(){
+        $$('.case.current').removeClass('current');
+        $('CurrentCase').destroy();
+    }
 });
+
+
+
